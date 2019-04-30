4fa64c3fd35ecad3c646f4976e05c237c06fe928
Mary Selig <seligmar@gmail.com>
2019-04-30 19:10:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,11 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed 
   end
 
   def name
     @name
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def default(breed)
-    @breed = "Mutt" 
   end
 
    def breed
